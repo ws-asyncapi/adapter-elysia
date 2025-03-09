8accb67749d21808d839c1759328a8e729fba395
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-03-10 00:02:38 +0300

    chore: use WebSocketElysia

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import { Elysia, t } from "elysia";
 import type { AnyChannel } from "ws-asyncapi";
+import { WebSocketElysia } from "./websocket.ts";
 
 export function wsAsyncAPIAdapter(channels: AnyChannel[]) {
-    const app = new Elysia({
-        name: "ws-asyncapi-adapter",
-    });
+	const app = new Elysia({
+		name: "ws-asyncapi-adapter",
+	});
 
-    for (const channel of channels) {
-        app.ws(channel.address, {
-            body: t.Tuple([t.String(), t.Any()]),
-            message: async (ws, message) => {
-                const [type, data] = message;
+	for (const channel of channels) {
+		app.ws(channel.address, {
+			body: t.Tuple([t.String(), t.Any()]),
+			message: async (ws, message) => {
+				const [type, data] = message;
 
-                const result = channel["~"].client.get(type);
-                if (!result) return console.warn(`No handler found for ${type}`);
+				const result = channel["~"].client.get(type);
+				if (!result) return console.warn(`No handler found for ${type}`);
 
-                await result.handler({ ws, message: data });
-            },
-        });
-    }
+				await result.handler({
+					ws: new WebSocketElysia(ws),
+					message: data,
+				});
+			},
+		});
+	}
 
-    return app;
+	return app;
 }
